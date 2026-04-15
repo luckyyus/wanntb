37992898ce69e61625a9b68611254f4fc3a2965e
luckyyus <jxyu@suda.edu.cn>
2026-04-15 12:52:00 +0800

    调整文件布局

--- examples/symm-GaAs.py
+++ examples/symm-GaAs.py
@@ -1,7 +1,7 @@
 import numpy as np
 import os
-import wanntb
-
+import wanntb as wtb
+import wanntb.symmetrize as sym
 
 # GaAs fcc
 path = 'GaAs-soc-001'
@@ -16,16 +16,16 @@ kpath = np.array([[0.50, 0.50, 0.50],
 
 npzfile = os.path.join('..','data', path + '-tb.npz')
 
-tb = wanntb.get_tbsystem_by_npz_file(npz_file=npzfile)
+tb = wtb.get_tbsystem_by_npz_file(npz_file=npzfile)
 
 tb.output_bands_kpath(kpath, nkpts_path=100, filename=path + '-bands-orig.txt')
 
-symm = wanntb.symmetrize.Symmetrizer(tb, magmom_str='0 0', is_soc=True)
+symm = sym.Symmetrizer(tb, magmom_str='0 0', is_soc=True)
 
 ham_out, r_mat_out, ss_out, r_vec = symm.symmetrize('h',
                                                     # enable_list=[1],
                                                     is_expand=True)
 # print(symm.site_maps[1])
-tb_new = wanntb.get_tbsystem_by_new_ham(tb, ham_out, r_mat_out, r_vec, ss_R_new=ss_out)
+tb_new = wtb.get_tbsystem_by_new_ham(tb, ham_out, r_mat_out, r_vec, ss_R_new=ss_out)
 #
 tb_new.output_bands_kpath(kpath, nkpts_path=100, filename=path + '-bands-symm.txt')
--- wanntb/__init__.py
+++ wanntb/__init__.py
@@ -1,11 +1,8 @@
 
 from ._system import get_tbsystem_by_new_ham, get_tbsystem_by_tb_file, get_tbsystem_by_npz_file
 
-__all__ = ['symmetrize', 'negf', 'utility', 'kpoints','constant']
+__all__ = ['utility', 'kpoints','constant']
 
 from . import utility
 from . import kpoints
 from . import constant
-
-from . import symmetrize
-from . import negf
